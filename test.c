3c54113898f9867250442fbc43042f2ebd861fc2
Balaji <balajivelu303@outlook.com>
2019-09-26 18:26:19 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Hello World");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=n;j>0;j--){
+            if(i==j)
+            printf("");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
