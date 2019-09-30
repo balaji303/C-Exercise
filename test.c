8bb02259ea317dc59de7b8cb1a71c8f507e51d4d
Balaji <balajivelu303@outlook.com>
2019-09-30 11:22:02 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    int i,j,n;
-    printf("Hello World");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for(j=n;j>0;j--){
-            if(i==j)
-            printf("");
+    char a[1000],b[1000],c[10];
+    int i;
+    // printf("Enter");
+    scanf("%s",b);
+    // for(i=0;a[i];i++){
+    // b[i]=a[i];
+    // // printf("%c\n",b[i]);
+    // }
+    for(i=0;i<10;i++){
+        c[i]=0;
+    }
+    for(i=0;b[i];i++){
+        if(b[i]=='0'){
+            c[0]=c[0]+1;
         }
-        printf("\n");
+        else if(b[i]=='1'){
+            c[1]=c[1]+1;
+        }
+        else if(b[i]=='2'){
+            c[2]=c[2]+1;
+        }
+        else if(b[i]=='3'){
+            c[3]=c[3]+1;
+        }
+        else if(b[i]=='4'){
+            c[4]=c[4]+1;
+        }
+        else if(b[i]=='5'){
+            c[5]=c[5]+1;
+        }
+        else if(b[i]=='6'){
+            c[6]=c[6]+1;
+        }
+        else if(b[i]=='7'){
+            c[7]=c[7]+1;
+        }
+        else if(b[i]=='8'){
+            c[8]=c[8]+1;
+        }
+        else if(b[i]=='9'){
+            c[9]=c[9]+1;
+        }
+        else{
+            
+        }
+    }
+    for(i=0;i<10;i++){
+        printf("%d ",c[i]);
     }
     return 0;
 }
-
