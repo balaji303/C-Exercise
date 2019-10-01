0445a5e967efa563d7112c3f5fbbb5823be7da8e
Balaji <balajivelu303@outlook.com>
2019-10-01 18:31:25 +0530

    Create 2DArrayasArgument.c

---
+++ 2DArrayasArgument.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void func(int a[][5]);
+int main(void){
+    int j,i,arr[2][5]={
+                  {1,2,3,4,5},
+                  {6,7,8,9,10}
+                  };
+    func(arr);
+    for(i=0;i<2;i++){
+        for(j=0;j<5;j++){
+        printf("%d ",arr[i][j]);
+     }
+     printf("\n");
+    }
+    return 0;
+}
+void func(int a[][5]){
+    int i,j;
+    for(i=0;i<2;i++){
+        for(j=0;j<5;j++){
+        a[i][j]=a[i][j]+5;
+        }
+    }
+}
