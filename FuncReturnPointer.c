4ccac935b261fcd873d024afd9a00f758ca5d508
Balaji <balajivelu303@outlook.com>
2019-10-03 14:38:36 +0530

    Update FuncReturnPointer.c

--- FuncReturnPointer.c
+++ FuncReturnPointer.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int *fun(void);
+int main(void){
+    
+    int *x;
+    x=fun();
+    printf("%d",*x);
+    return 0;
 
+    
+}
+int *fun(void){
+    int a=5;
+    int *ptr;
+    ptr=&a;
+    return ptr;
+
+    
+}
