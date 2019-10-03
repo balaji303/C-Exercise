b5c57b07be9c96ff2134aa6e56c2aa0548537f4a
Balaji <balajivelu303@outlook.com>
2019-10-03 14:19:32 +0530

    Update FuncReturnPointer.c

--- FuncReturnPointer.c
+++ FuncReturnPointer.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-void fun(int x,int y,int *ps,int *pd,int *pm);
-int main(){
-    int a,b,sum,diff,mul;
-    printf("Enter value for a:");
-    scanf("%d",&a);
-    printf("Enter value for b:");
-    scanf("%d",&b);
-    fun(a,b,&sum,&diff,&mul);
-    printf("Sum=%d\tDifference=%d\tMultiplication=%d",sum,diff,mul);
-    return 0;
-}
-void fun(int x,int y,int *ps,int *pd,int *pm){
-    *ps=x+y;
-    *pd=x-y;
-    *pm=x*y;
-}
+
