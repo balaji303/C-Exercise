437c2c5a6d2a3a4f85df62287eddcd99b01b491e
Balaji <balajivelu303@outlook.com>
2019-12-26 14:26:44 +0530

    Update clockangle.c

--- clockangle.c
+++ clockangle.c
@@ -17,7 +17,7 @@ while(min>=61 || hour<=0)
   printf("Wrong Minute Please enter between 1 to 60 ");
   scanf("%d",&min);
 }
-	//printf("The Enered Hour and the minute 
+	//printf
 cmin=min/5;
 chour=hour;
 if(hour>=cmin)
