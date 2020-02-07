95a8d8780684e54c7d95f74a17be30e05720bfc0
Balaji <balajivelu303@outlook.com>
2020-02-07 16:16:45 +0530

    Update StringManipulate.c

--- StringManipulate.c
+++ StringManipulate.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    //Pointer
+    char *p="Balaji"; //Only one byte is given
+    p=malloc(7);      //7=6+1(''\0') byte given
+    strcpy(p,"Balaji");//copy string
+    //Array 
+    char arr[10]={'B','a','l','a','j','i'};
+    arr[2]='x'; //changing elements 
+    *(p+2)='z'; //changing elements
+    puts(p);    //print
+    puts(arr);  //print
+    free(p);    //free memory because of dynamic allogation
+    return 0;
+}
 
