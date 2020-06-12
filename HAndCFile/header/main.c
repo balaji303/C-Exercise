8d1dc0a66d96aca151fa6529a2ef46eb3e8ca9cf
Balaji <balajivelu303@outlook.com>
2020-06-12 16:54:01 +0530

    Update main.c

--- HAndCFile/header/main.c
+++ HAndCFile/header/main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include "oneTwoThree.h"
 int main()
