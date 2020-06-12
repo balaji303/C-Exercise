7062ff3da75f5d7b2242ba54b045592057a5923f
Balaji <balajivelu303@outlook.com>
2020-06-12 16:51:50 +0530

    Add files via upload

---
+++ HAndCFile/header/main.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include "oneTwoThree.h"
+int main()
+{
+    int a;
+    printf("Hello World\n");
+    first(5);
+    return 0;
+}
