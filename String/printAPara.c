112470f199c13b3ed2317902f274ef92cd04fc49
Balaji <balajivelu303@outlook.com>
2020-08-26 18:21:16 +0530

    Update printAPara.c

--- String/printAPara.c
+++ String/printAPara.c
@@ -1,1 +1,10 @@
-
+/*Printing a para*/
+#include <stdio.h>
+#include <string.h>
+int main(void){
+char str1[100];
+printf("Enter a string(Press Enter to terminate)\n");
+scanf("%[^\n]s",str1);
+	printf("%s",str1);
+return 0;
+}
