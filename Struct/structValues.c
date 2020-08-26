58ae704be667aee1a00a066d52876f8ac9e25d37
Balaji <balajivelu303@outlook.com>
2020-08-26 18:26:20 +0530

    Update structValues.c

--- Struct/structValues.c
+++ Struct/structValues.c
@@ -1,1 +1,15 @@
+/*
+Structure is given a whole values to the variable
+*/
+#include <stdio.h>
+typedef struct{
+  int rollno;
+  char name[10];
+  float fees;
+}student;
+student std1={1,"balaji",2.3};//Values at whole can be given only at the time of declaration
+int main(void){
+ printf("%d",std1.rollno);
+ return 0;
+}
 
