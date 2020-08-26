36707f81b15496e725ef93c16831001930255e02
Balaji <balajivelu303@outlook.com>
2020-08-26 16:28:59 +0530

    Update digitsToNumber.c

--- Problems/digitsToNumber.c
+++ Problems/digitsToNumber.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+int value,Total;
+char size;
+printf("Enter Total Digit:");
+    scanf("%d",&size);
+for(int count=1;count<=size;count++){
+     printf("Enter %d\'s digit:",count);
+scanf("%d",value);
+Total=(value*10)+value;
+}
+printf("Value is :%d",Total);
+return 0;
+}
 
+/*
+ip:
+2
+3
+4
+
+op:
+234
+*/
