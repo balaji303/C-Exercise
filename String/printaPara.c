306f949def302c2f60dd9cf1b9e45dda90ce9a5d
Balaji <balajivelu303@outlook.com>
2020-08-26 18:20:18 +0530

    Update printaPara.c

--- String/printaPara.c
+++ String/printaPara.c
@@ -1,1 +1,10 @@
-
+/*Printing a para*/
+#include <stdio.h>
+#include <string.h>
+int main(void){
+char str1[100];
+printf("Enter a string(Press tab to terminate)\n");
+scanf("%[^\t]s",str1);
+	printf("%s",str1);
+return 0;
+}
