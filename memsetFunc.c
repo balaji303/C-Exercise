d73d1d734d677aae04ac7c37c86fd76e2c2a58a9
balaji303 <balajivelu303@outlook.com>
2021-04-15 21:00:21 +0530

    ThurNi

--- memsetFunc.c
+++ memsetFunc.c
@@ -13,3 +13,22 @@ int main(){
     printf("\n%s",val);
     return 0;
 }
+/*
+// C program to demonstrate working of memset()
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char str[27] = "abcdefghijklmnopqrstuvwxyz.";
+	printf("\nBefore memset(): %s\n", str);
+
+	// Fill 8 characters starting from str[13] with '.'
+	memset(str+8, '@', 8*sizeof(char));
+
+	printf("After memset(): %s", str);
+	return 0;
+}
+
+*/
+
